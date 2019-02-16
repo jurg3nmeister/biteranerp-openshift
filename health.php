a6fff1612c22bf044210c64350cfca156c131989
Jurgen Cuiza <jurgen.cuiza@biteran.com>
2019-02-16 14:13:35 -0400

    testlast

--- health.php
+++ health.php
@@ -12,11 +12,7 @@ $connection = pg_connect($conn_string);
 // Check connection
 if($connection) {   
   	echo "OK \n";
-  	echo $servername."\n";
-  	echo $dbname."\n";
-  	echo $username."\n";
-  	echo $password."\n";
-  	echo "pichanga"; 
+  	echo $conn_string;
 }else{
 	header("HTTP/1.1 503 Service Unavailable");
   	die("Connection failed: " . $connection);
