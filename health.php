4a8cdfbcac459bc81b1f586da0cfa06e02c17541
Jurgen Cuiza <jurgen.cuiza@biteran.com>
2019-02-16 13:43:49 -0400

    test11

--- health.php
+++ health.php
@@ -1,11 +1,12 @@
 <?php
 
 $servername = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
+$dbname = getenv("DATABASE_NAME");
 $username = getenv("DATABASE_USER");
 $password = getenv("DATABASE_PASSWORD");
 
 // Create connection
-$conn_string = "host=".$servername." user=".$username." password=".$password."";
+$conn_string = "host=".$servername." dbname=".$$dbname." user=".$username." password=".$password."";
 echo $conn_string;
 //$connection = pg_connect($conn_string);
 
