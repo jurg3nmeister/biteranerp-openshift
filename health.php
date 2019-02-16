c9890960d836664ac6725d0e2bc69be45a4cf793
Jurgen Cuiza <jurgen.cuiza@biteran.com>
2019-02-16 14:03:56 -0400

    test15

--- health.php
+++ health.php
@@ -1,25 +1,25 @@
 <?php
 
 $servername = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
-$dbname = "biteranerp";
+$dbname = getenv("DATABASE_NAME");
 $username = getenv("DATABASE_USER");
 $password = getenv("DATABASE_PASSWORD");
 
-// Create connection
-$conn_string = "host=".$servername." dbname=biteranerp user=".$username." password=".$password."";
-//echo $conn_string;
-$connection = pg_connect($conn_string);
+// // Create connection
+$conn_string = "host=".$servername." dbname=".$dbname." user=".$username." password=".$password."";
+echo $conn_string;
+// $connection = pg_connect($conn_string);
 
-// Check connection
-if($connection) {   
-  	echo "OK \n";
-  	echo $servername."\n";
-  	echo $dbname."\n";
-  	echo $username."\n";
-  	echo $password."\n";
-  	echo "pichanga"; 
-}else{
-	header("HTTP/1.1 503 Service Unavailable");
-  	die("Connection failed: " . $connection);
-}
+// // Check connection
+// if($connection) {   
+//   	echo "OK \n";
+//   	echo $servername."\n";
+//   	echo $dbname."\n";
+//   	echo $username."\n";
+//   	echo $password."\n";
+//   	echo "pichanga"; 
+// }else{
+// 	header("HTTP/1.1 503 Service Unavailable");
+//   	die("Connection failed: " . $connection);
+// }
 ?>
